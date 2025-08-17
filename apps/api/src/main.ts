4ec3e37e4681ffcbf80a5f8f092761943d793344
Austin Cory Bart <acbart@vt.edu>
2025-08-17 18:32:29 -0400

    Need to bind to the correct port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || undefined;
+  await app.listen(port, host);
 }
 
 void bootstrap();
