a0169a4c7750328b75020a76cc219b88d33bdcd9
rickykiamilev <rickyk@udel.edu>
2025-11-10 00:46:55 -0500

    updated users nestjs dto folder added

--- packages/api/src/links/entities/link.entity.ts
+++ packages/api/src/links/entities/link.entity.ts
@@ -6,7 +6,7 @@ export class Link {
 
   // Don't really want this, delete this whole file. Just left it in as an example.
   constructor() {
-  this.id = 0;
+    this.id = 0;
     this.url = '';
     this.title = '';
     this.description = '';
