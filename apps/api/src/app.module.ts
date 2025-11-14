e730a8c60e64bcb6c0c8e4660afcd529e56417ec
rickykiamilev <rickyk@udel.edu>
2025-11-14 16:17:20 -0500

    rm links from app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { LinksModule } from './links/links.module'
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-
-import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
+  
 
 @Module({
-  imports: [LinksModule, UsersModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -5,6 +5,6 @@ import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
