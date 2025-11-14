c63a814f2a8d8e17c7873972f65fd91e71964068
rickykiamilev <rickyk@udel.edu>
2025-11-14 15:26:10 -0500

    commit local branch again

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,8 +5,12 @@ import { LinksModule } from './links/links.module'
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
+import { PrismaService } from './prisma.service';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+
 @Module({
-  imports: [LinksModule],
+  imports: [LinksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
